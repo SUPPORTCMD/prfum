94e4e40174ee138ac62afb699dac7836e57fbb6d
SUPPORTCMD <skh1xbet@gmail.com>
2026-07-23 17:10:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   databaseURL: "https://prfum-b8275-default-rtdb.firebaseio.com/"
@@ -8,5 +9,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
-export { db, ref, set, push, onValue, remove, update };
+export { db, ref, set, push, onValue, remove, update, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut };
