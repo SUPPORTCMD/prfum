7d336ed92851e5d51d4a07bf46abe98f1290a83a
SUPPORTCMD <skh1xbet@gmail.com>
2026-07-23 19:10:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getDatabase, ref, set, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 🔥🔥🔥 بيانات Firebase الخاصة بك (من اللقطة اللي بعتها) 🔥🔥🔥
+// 🔥🔥🔥 بياناتك بالضبط (من اللقطة اللي بعتها) 🔥🔥🔥
 const firebaseConfig = {
   apiKey: "AIzaSyBvNcTEgRq_ghnGNnkTzF39q0fz-xnZbts",
   authDomain: "prfum-b8275.firebaseapp.com",
@@ -15,6 +15,7 @@ const firebaseConfig = {
   measurementId: "G-2428ZKY9LR"
 };
 
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
